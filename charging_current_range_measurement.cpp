8b1e6e201f6d5737484bbf978fbdf411895ee0a0
deepakp-31 <deepakp3101@gmail.com>
2023-02-08 00:50:34 +0530

    Added sort and sequence

--- charging_current_range_measurement.cpp
+++ charging_current_range_measurement.cpp
@@ -1,8 +1,38 @@
 #include "charging_current_range_measurement.hpp"
 
-std::string testChargingCurrentMeasurement(std::vector<int> chargingCurrentReadings)
+// Given a vector
+//     Sort it
+//     Find sequence
+//         its upper and lower limit
+
+//     Check the number of occurance of each element in the sequnce
+
+std::vector<std::vector<int>> sequenceChargingCurrentReadings(std::vector<int> f_sorted_charging_current_readings)
 {
-    std::string checks_measure = std::to_string(chargingCurrentReadings.at(0)) + "-" + std::to_string(chargingCurrentReadings.at(1)) + ", " + std::to_string(chargingCurrentReadings.size());
+    std::vector<std::vector<int>> sequence_charging_current_readings;
+    std::vector<int> sequence_charging_current_reading;
+
+    for (auto current_reading:f_sorted_charging_current_readings)
+    {
+        
+    }
+}
+
+std::vector<int> sortChargingCurrentReadings(std::vector<int> f_charging_current_readings)
+{
+    sort(f_charging_current_readings.begin(), f_charging_current_readings.end());
+    return f_charging_current_readings;
+}
+
+std::string testChargingCurrentMeasurement(std::vector<int> f_charging_current_readings)
+{
+    // Sorting
+    std::vector<int> sorted_charging_current_readings = sortChargingCurrentReadings(f_charging_current_readings);
+
+    // Find Sequence
+    std::vector<std::vector<int>> sequence_charging_current_readings = sequenceChargingCurrentReadings(sorted_charging_current_readings);
+
+    std::string checks_measure = std::to_string(f_charging_current_readings.at(0)) + "-" + std::to_string(f_charging_current_readings.at(1)) + ", " + std::to_string(f_charging_current_readings.size());
 
     return checks_measure;
 }
--- charging_current_range_measurement.hpp
+++ charging_current_range_measurement.hpp
@@ -4,5 +4,8 @@
 #include <map>
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
+
+std::vector<int> sortChargingCurrentReadings(std::vector<int> f_charging_current_readings);
 
 std::string testChargingCurrentMeasurement(std::vector<int> chargingCurrentReadings);
