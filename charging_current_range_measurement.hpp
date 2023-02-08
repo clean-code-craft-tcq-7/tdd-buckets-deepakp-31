afdb5e4eb2b1ad6de5ba517314c2d75474034f3a
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-08 20:34:30 +0530

    Update charging_current_range_measurement.hpp

--- charging_current_range_measurement.hpp
+++ charging_current_range_measurement.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <algorithm>
 
+std::vector<std::vector<int>> sequenceChargingCurrentReadings(std::vector<int> f_sorted_charging_current_readings);
+
 std::vector<int> sortChargingCurrentReadings(std::vector<int> f_charging_current_readings);
 
-std::string testChargingCurrentMeasurement(std::vector<int> chargingCurrentReadings);
+std::map<std::string, int> testChargingCurrentMeasurement(std::vector<int> chargingCurrentReadings);
