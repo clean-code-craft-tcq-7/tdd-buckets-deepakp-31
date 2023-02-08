42bf64e3ec741df2e0c6d4a00ef2f1699886f550
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-08 22:04:15 +0530

    Update charging_current_range_measurement.cpp

--- charging_current_range_measurement.cpp
+++ charging_current_range_measurement.cpp
@@ -12,8 +12,15 @@ std::vector<std::vector<int>> sequenceChargingCurrentReadings(std::vector<int> f
     std::vector<std::vector<int>> sequence_charging_current_readings;
     std::vector<int> sequence_charging_current_reading;
 
-    for (auto current_reading : f_sorted_charging_current_readings)
+    int temp = f_sorted_charging_current_readings.at(0);
+    for (int i = 0; i < f_sorted_charging_current_readings.size() - 1; i++)
     {
+
+        if (!(abs((f_sorted_charging_current_readings.at(i + 1) - (f_sorted_charging_current_readings.at(i)))) <= 1))
+        {
+            
+            temp = f_sorted_charging_current_readings.at(i);
+        }
     }
 }
 
@@ -23,18 +30,25 @@ std::vector<int> sortChargingCurrentReadings(std::vector<int> f_charging_current
     return f_charging_current_readings;
 }
 
-
 std::map<std::string, int> testChargingCurrentMeasurement(std::vector<int> f_charging_current_readings)
 {
+    std::map<std::string, int> current_charging_range;
+
+    std::vector<int> sorted_charging_current_readings;
+
+    int charging_current_reading;
+
     // Sorting
-    std::vector<int> sorted_charging_current_readings = sortChargingCurrentReadings(f_charging_current_readings);
+    sorted_charging_current_readings = sortChargingCurrentReadings(f_charging_current_readings);
 
     // Find Sequence
     std::vector<std::vector<int>> sequence_charging_current_readings = sequenceChargingCurrentReadings(sorted_charging_current_readings);
+
+    current_charging_range[std::to_string(sequence_charging_current_readings)] = charging_current_reading;
 
     std::map<std::string, int> checks_measure = std::to_string(f_charging_current_readings.at(0)) + "-" + std::to_string(f_charging_current_readings.at(1)) + ", " + std::to_string(f_charging_current_readings.size());
 
     // return checks_measure;
 
-      return {{"4-5", 2}};
+    return {{"4-5", 2}};
 }
