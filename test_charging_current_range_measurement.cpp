6fcf97e753d02a5368fefa4e98fa767f630877b8
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-08 20:19:04 +0530

    Update test_charging_current_range_measurement.cpp

--- test_charging_current_range_measurement.cpp
+++ test_charging_current_range_measurement.cpp
@@ -2,7 +2,10 @@
 #include "test/catch.hpp"
 #include "charging_current_range_measurement.hpp"
 
+std::vector<int> current_charging_range = {4, 5};
+std::map<std::string, int> continous_range = {{4, 5, 2}};
+
 TEST_CASE("Test Charging Current Measurement")
 {
-    REQUIRE(testChargingCurrentMeasurement({4, 5}) == "4-5, 2");
+    REQUIRE(testChargingCurrentMeasurement(current_charging_range) == continous_range);
 }
