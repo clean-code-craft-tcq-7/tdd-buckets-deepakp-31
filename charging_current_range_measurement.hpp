e1c910aba31dc56b022a33b5c8f7e0cb40d0ae4d
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-10 12:18:43 +0530

    Update charging_current_range_measurement.hpp

--- charging_current_range_measurement.hpp
+++ charging_current_range_measurement.hpp
@@ -10,34 +10,23 @@
 #include <algorithm>
 #include <cmath>
 
-int reference_current = 10;
-int A2D_12_bit_converter_bit = 12;
-int min_12_bit_digital_current = 0;
-int max_12_bit_digital_current = 4094;
+// A2D Converted
+int A2DConverter(int& f_rounded_current_reading_in_amps, int& f_reference_value, int& f_A2D_converter_bit);
 
-int max_reference_current = 15;
-int min_reference_current = -15;
-int A2D_10_bit_converter_bit = 10;
-int min_10_bit_digital_current = 0;
-int max_10_bit_digital_current = 1022;
-
-int A2D10BitConverter(int f_rounded_current_reading_in_amps);
-
-int A2DConverter(int f_rounded_current_reading_in_amps);
-
-bool isValidA2DConvertedCurrentMeasurement(int &f_digital_current_reading, int &f_min_current, int &f_max_current);
+//Checking validity of an A2D converted current measurement
+bool isValidA2DConvertedCurrentMeasurement(int& f_digital_current_reading, int& f_min_current, int& f_max_current);
 
 // Function if last element in the vector
-bool isLastCurrentMeasurement(const int &i, const int &f_size_of_current_charging_reading);
+bool isLastCurrentMeasurement(const int& i, const int& f_size_of_current_charging_reading);
 
 // Function to update each charging Range & Readings
-void updateCurrentChargingRangeAndReadings(std::vector<int> &f_current_charging_sequence,
-                                           std::map<std::string, int> &f_current_charging_readings_sequence);
+void updateCurrentChargingRangeAndReadings(std::vector<int>& f_current_charging_sequence,
+                                           std::map<std::string, int>& f_current_charging_readings_sequence);
 
 // Function to create the map with sequence of multiple current readings
 std::map<std::string, int> sequenceOfMultipleChargingCurrentReadings(
-    std::vector<int> &f_current_charging_sequence, std::map<std::string, int> &f_current_charging_readings_sequence,
-    std::vector<int> &f_sorted_charging_current_readings);
+    std::vector<int>& f_current_charging_sequence, std::map<std::string, int>& f_current_charging_readings_sequence,
+    std::vector<int>& f_sorted_charging_current_readings);
 
 // Function to create the map with sequence of current readings
 std::map<std::string, int> sequenceChargingCurrentReadings(std::vector<int> f_sorted_charging_current_readings);
